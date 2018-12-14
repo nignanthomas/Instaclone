e8f2c2dee218abfdfd2d46b4095dbe66378b1d30
nignanthomas <nignanthomas@gmail.com>
2018-12-14 15:51:11 +0300

    create Profile model

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from tinymce.models import HTMLField
-# from django.db.models import Q
+from tinymce.models import HTMLField
+from django.db.models import Q
 
 Gender=(
     ('Male','Male'),
@@ -20,3 +20,17 @@ class Location(models.Model):
     @classmethod
     def delete_location(cls,location):
         cls.objects.filter(location=location).delete()
+
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to='profilepics/')
+    fullname = models.CharField(max_length=255)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    bio = HTMLField()
+    email = models.EmailField()
+    phonenumber = models.IntegerField()
+    gender = models.CharField(max_length=15,choices=Gender,default="Male")
+
+    def __str__(self):
+        return self.username
+
+    
