b8963133fa6646b25ebe412d4f8a0a8efa1cf397
nignanthomas <nignanthomas@gmail.com>
2018-12-14 15:29:24 +0300

    create model Location

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+# from django.contrib.auth.models import User
+# from tinymce.models import HTMLField
+# from django.db.models import Q
 
+Gender=(
+    ('Male','Male'),
+    ('Female','Female'),
+)
 # Create your models here.
+class Location(models.Model):
+    location_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.location_name
+
+    def save_location(self):
+        self.save()
+
+    @classmethod
+    def delete_location(cls,location):
+        cls.objects.filter(location=location).delete()
