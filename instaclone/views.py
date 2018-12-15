5f0ce8520290044b284d87348ffc7e95ab4f5400
nignanthomas <nignanthomas@gmail.com>
2018-12-15 11:57:35 +0300

    create timeline view function; define instaclone.urls.py; create templates

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Post,Profile
+# from .forms import PostForm,LocationForm,ProfileForm,CommentForm
+# from django.http import JsonResponse
+# import json
+from django.db.models import Q
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def timeline(request):
+    current_user=request.user
+    posts= Post.objects.all()
+    profiles= Profile.objects.all()
+    # form=CommentForm()
+    # comments=Comment.objects.all()
+    return render(request,'timeline.html',{"posts":posts,"profiles":profiles})
--- instagram/settings.py
+++ instagram/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'instaclone',
+    'bootstrap4',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
