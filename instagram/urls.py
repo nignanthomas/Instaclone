eb10dfc3af8c71e5b7c2b8b790685765374fadb7
nignanthomas <nignanthomas@gmail.com>
2018-12-16 08:17:53 +0300

    add accounts url to instagram/urls.py

--- instagram/urls.py
+++ instagram/urls.py
@@ -21,5 +21,6 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('instaclone.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 
 ]
