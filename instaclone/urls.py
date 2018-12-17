eead3d878abdafec060f9abe928f8820709dc0cc
nignanthomas <nignanthomas@gmail.com>
2018-12-17 12:42:45 +0300

    update navbar

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.timeline,name='timeline'),
     url(r'^search/', views.search_results, name = 'search_results'),
-    url(r'^explore/', views.explore, name = 'expolre'),
+    url(r'^explore/', views.explore, name = 'explore'),
     url(r'^profile/', views.profile, name = 'profile'),
 
 ]
